e5a09fd69d8487782592d81a1110a72e3cdbf098
Ekaterina Zdanovich <zdanovichkatsiaryna@gmail.com>
2025-10-26 22:36:08 +0300

    Homework #14

--- src/main/kotlin/Lessons/lesson14/homeworks/Party.java
+++ src/main/kotlin/Lessons/lesson14/homeworks/Party.java
@@ -1,14 +1,13 @@
 package lessons.lesson14.homeworks;
 
-import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
 import static java.sql.DriverManager.println;
 
-public class HW14Party {
+
     //Событие: вечеринка. Создайте класс Party, который описывает вечеринку.
     // У него должны быть свойства location (String) и attendees (Int). Добавьте метод details(),
     // который выводит информацию о месте проведения и количестве гостей.
 
-    class Party(val location: String, val attendees: Int) {
+class Party(val location: String, val attendees: Int) {
         fun details() {
             println("The party is at $location with $attendees attendees.")
         }
